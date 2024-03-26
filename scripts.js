235a8df2674beb0ff43d9b348446533bb1721c7a
srpntspth <155493228+srpntspth@users.noreply.github.com>
2024-03-26 18:00:40 -0400

    v2 (added css + functional buttons)

--- scripts.js
+++ scripts.js
@@ -5,36 +5,77 @@ let gridArr = gridArray(gridSize);
 let drawWith = "mouse";
 
 let brush = false;
-document.addEventListener("keydown", function () {
+document.querySelector(".brush").addEventListener("click", function () {
     if (brush == false) {
         brush = true;
+        eyedropper = false;
     } else if (brush == true) {
         brush = false;
-    };
+    }
 });
 
 let eraser = false;
 document.querySelector(".eraser").addEventListener("click", function () {
     if (eraser == false) {
         eraser = true;
+        brush = false;
+        eyedropper = false;
     } else if (eraser == true) {
         eraser = false;
+        brush = true;
+        eyedropper = false;
     }
 });
 
-let color = "black";
+let eyedropper = false;
+document.querySelector(".eyedropper").addEventListener("click", function () {
+    if (eyedropper == false) {
+        eyedropper = true;
+        brush = false;
+        eraser = false;
+    } else if (eyedropper == true) {
+        eyedropper = false;
+    }
+    console.log(eyedropper);
+});
+
+let color;
 document.querySelector(".random").addEventListener("click", function () {
-    color = `rgb(${Math.floor((Math.random() * (255 - 1)))}, ${Math.floor((Math.random() * (255 - 1)))}, ${Math.floor((Math.random() * (255 - 1)))})`;
-    console.log(color);
-})
+    setColor(Math.floor((Math.random() * (255 - 1))), 
+             Math.floor((Math.random() * (255 - 1))), 
+             Math.floor((Math.random() * (255 - 1))));
+});
+
 
 createGrid();
 console.log(gridArr[4][20]);
 
 
+let r = document.querySelector("#r");
+let g = document.querySelector("#g");
+let b = document.querySelector("#b");
+r.addEventListener("change", function () {
+    setColor(r.value, g.value, b.value);
+})
+g.addEventListener("change", function () {
+    setColor(r.value, g.value, b.value);
+})
+b.addEventListener("change", function () {
+    setColor(r.value, g.value, b.value);
+})
+
+function setColor(rV, gV, bV) {
+    r.value = rV;
+    g.value = gV;
+    b.value = bV;
+    color = `rgb(${rV}, ${gV}, ${bV})`;
+    document.querySelector(".colorBox").style.backgroundColor = color;
+    console.log(color);
+}
+
 function createGrid() {
     for (let i = 0; i < gridSize; i++) {
-        let gridCol = document.createElement("col");
+        let gridCol = document.createElement("div");
         gridCol.classList.add("col");
 
         for (let j = 0; j < gridSize; j++) {
@@ -46,15 +87,21 @@ function createGrid() {
 
             gridCell.addEventListener("mouseenter", function () {
                 if (brush == true) {
-                    if (eraser == false) {
-                        gridCell.style.border = `1px solid ${color}`
-                        gridCell.style.backgroundColor = `${color}`;
-                    } else if (eraser == true) {
-                        gridCell.style.border = "1px solid rgb(198, 198, 198)";
-                        gridCell.style.backgroundColor = "white";
-                    }
+                    gridCell.style.border = `0.5px solid ${color}`;
+                    gridCell.style.backgroundColor = `${color}`;
                 }
-            });
+                if (eraser == true) {
+                    gridCell.style.border = "0.5px solid rgb(255, 255, 255)";
+                    gridCell.style.backgroundColor = "white";
+                }
+                if (eyedropper == true) {
+                    gridCell.addEventListener("click", function () {  
+                        let colorID = gridCell.style.backgroundColor.slice(4, 17).split(", ");
+                        console.log(colorID);
+                        setColor(parseInt(colorID[0]), parseInt(colorID[1]), parseInt(colorID[2]));
+                    })
+                }
+            })
         }
         grid.appendChild(gridCol);
     }
