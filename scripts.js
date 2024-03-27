5c4603747f19af9a4284501ea438d310e274bb2e
srpntspth <155493228+srpntspth@users.noreply.github.com>
2024-03-26 23:04:29 -0400

    v3 - added button toggling

--- scripts.js
+++ scripts.js
@@ -2,41 +2,69 @@ const grid = document.querySelector(".grid");
 const gridSize = 60;
 
 let gridArr = gridArray(gridSize);
-let drawWith = "mouse";
 
 let brush = false;
-document.querySelector(".brush").addEventListener("click", function () {
+let brushBtn = document.querySelector(".brush");
+brushBtn.addEventListener("click", function () {
     if (brush == false) {
         brush = true;
+        eraser = false;
         eyedropper = false;
+        btnStyle(eraser, eraserBtn, "default");
+        btnStyle(eyedropper, eyedropperBtn, "default");
     } else if (brush == true) {
         brush = false;
+        eyedropper = false;
     }
+});
+brushBtn.addEventListener("mouseenter", function () {
+    btnStyle(brush, brushBtn, "mouseenter");
+});
+brushBtn.addEventListener("mouseleave", function () {
+    btnStyle(brush, brushBtn, "mouseleave");
 });
 
 let eraser = false;
-document.querySelector(".eraser").addEventListener("click", function () {
+let eraserBtn = document.querySelector(".eraser");
+eraserBtn.addEventListener("click", function () {
     if (eraser == false) {
         eraser = true;
         brush = false;
         eyedropper = false;
+        btnStyle(brush, brushBtn, "default");
+        btnStyle(eyedropper, eyedropperBtn, "default");
     } else if (eraser == true) {
         eraser = false;
         brush = true;
         eyedropper = false;
     }
 });
+eraserBtn.addEventListener("mouseenter", function () {
+    btnStyle(eraser, eraserBtn, "mouseenter");
+});
+eraserBtn.addEventListener("mouseleave", function () {
+    btnStyle(eraser, eraserBtn, "mouseleave");
+});
+
 
 let eyedropper = false;
-document.querySelector(".eyedropper").addEventListener("click", function () {
+let eyedropperBtn = document.querySelector(".eyedropper");
+eyedropperBtn.addEventListener("click", function () {
     if (eyedropper == false) {
         eyedropper = true;
         brush = false;
         eraser = false;
+        btnStyle(brush, brushBtn, "default");
+        btnStyle(eraser, eraserBtn, "default");
     } else if (eyedropper == true) {
         eyedropper = false;
     }
-    console.log(eyedropper);
+});
+eyedropperBtn.addEventListener("mouseenter", function () {
+    btnStyle(eyedropper, eyedropperBtn, "mouseenter");
+});
+eyedropperBtn.addEventListener("mouseleave", function () {
+    btnStyle(eyedropper, eyedropperBtn, "mouseleave");
 });
 
 let color;
@@ -46,23 +74,18 @@ document.querySelector(".random").addEventListener("click", function () {
              Math.floor((Math.random() * (255 - 1))));
 });
 
-
-createGrid();
-console.log(gridArr[4][20]);
-
-
 let r = document.querySelector("#r");
 let g = document.querySelector("#g");
 let b = document.querySelector("#b");
 r.addEventListener("change", function () {
     setColor(r.value, g.value, b.value);
-})
+});
 g.addEventListener("change", function () {
     setColor(r.value, g.value, b.value);
-})
+});
 b.addEventListener("change", function () {
     setColor(r.value, g.value, b.value);
-})
+});
 
 function setColor(rV, gV, bV) {
     r.value = rV;
@@ -117,3 +140,29 @@ function gridArray(size) {
     }
     return array;
 }
+
+function btnStyle(toggle, btn, state) {
+    if (state == "mouseenter") {
+        if (toggle == false) {
+            btn.style.backgroundColor = "#508f59";
+        } else if (toggle == true) {
+            btn.style.backgroundColor = "#854c67";
+        }
+    }
+    if (state == "mouseleave") {
+        if (toggle == false) {
+            btn.style.backgroundColor = "#7fd496";
+        } else if (toggle == true) {
+            btn.style.backgroundColor = "#ad6386";
+        }
+    }
+    if (state == "default") {
+        if (toggle == false) {
+            btn.style.backgroundColor = "#7fd496";
+        } else if (toggle == true) {
+            btn.style.backgroundColor = "#ad6386";
+        }
+    }
+}
+
+createGrid();
